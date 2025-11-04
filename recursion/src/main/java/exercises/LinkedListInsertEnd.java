ca676530d6d89d9e1d02ebb057b39acf7234e330
sonyaginzburg <sginzbur@student.touro.edu>
2025-11-04 17:12:47 -0500

    Todos completed for implement recursively

--- recursion/src/main/java/exercises/LinkedListInsertEnd.java
+++ recursion/src/main/java/exercises/LinkedListInsertEnd.java
@@ -24,9 +24,13 @@ public class LinkedListInsertEnd {
 
     public static <T> Node<T> insertAtEnd(Node<T> head, T value) {
         // TODO: implement recursively
-        // Base 1: if (head == null) return new Node<>(value);
-        // Base 2: if (head.next == null) { head.next = new Node<>(value); return head; }
-        // Recursive: head.next = insertAtEnd(head.next, value); return head;
-        return head; // TEMP: replace with your recursive solution
+        if (head == null) return new Node<>(value);
+        if (head.next == null) {
+            head.next = new Node<>(value);
+            return head;
+        }
+        head.next = insertAtEnd(head.next, value);
+        return head;
+        //return head; // TEMP: replace with your recursive solution
     }
 }
